1630fd7030c55631bd53ea673ef97ef990b822e2
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-09 09:21:44 +0200

    Homework 1.2

--- src/Window.java
+++ src/Window.java
@@ -3,4 +3,9 @@ public class Window {
     String curtainColour;
     double tempIsolation;
 
+    Window(boolean isOpen, String curtainColour, double tempIsolation) {
+        this.isOpen = isOpen;
+        this.curtainColour = curtainColour;
+        this.tempIsolation = tempIsolation;
+    }
 }
--- src/WindowTest.java
+++ src/WindowTest.java
@@ -1,14 +1,14 @@
 public class WindowTest {
     public static void main(String[] args) {
-        Window window1 = new Window();
-        window1.isOpen = false;
-        window1.curtainColour = "blue";
-        window1.tempIsolation = 10;
+        Window window1 = new Window(false, "blue", 10);
+//        window1.isOpen = false;
+//        window1.curtainColour = "blue";
+//        window1.tempIsolation = 10;
 
-        Window window2 = new Window();
-        window2.isOpen = true;
-        window2.curtainColour = "white";
-        window2.tempIsolation = 14;
+        Window window2 = new Window(true, "white", 14);
+//        window2.isOpen = true;
+//        window2.curtainColour = "white";
+//        window2.tempIsolation = 14;
 
         System.out.println("Okno 1 jest otwarte ? " + window1.isOpen + " , posiada zasłonę w kolorze: " + window1.curtainColour + ", wsp. przenikalności cieplnej okna to: " + window1.tempIsolation );
         System.out.println("Okno 2 jest otwarte ? " + window2.isOpen + " , posiada zasłonę w kolorze: " + window2.curtainColour + ", wsp. przenikalności cieplnej okna to: " + window2.tempIsolation );
